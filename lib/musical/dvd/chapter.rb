384eefb125e343ccc76e8ab913f4a425c138a488
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2020-10-30 16:31:56 +0000

    added test case

--- lib/musical/dvd/chapter.rb
+++ lib/musical/dvd/chapter.rb
@@ -15,4 +15,5 @@ module Musical
       @vob_path = vob_path
       @name = options[:name] || DEFAULT_CHAPTER_NAME
       @chapter_number = options[:chapter_number] || DEFAULT_CHAPTER_NUMBER
-      @title_number = options[:title_number] || DEF
+      @title_number = options[:title_number] || DEFAULT_TITLE_NUMBER
+    end
