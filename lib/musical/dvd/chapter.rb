53c4947437378d0282550bf24105098e0e363750
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2020-11-03 17:25:36 +0000

    add fixme

--- lib/musical/dvd/chapter.rb
+++ lib/musical/dvd/chapter.rb
@@ -18,4 +18,7 @@ module Musical
       @title_number = options[:title_number] || DEFAULT_TITLE_NUMBER
     end
 
-    def to_wav(wav_path = "#{Musical.configuration.output}/chapter_#{@title_number}_#{@chapter_number}.wav
+    def to_wav(wav_path = "#{Musical.configuration.output}/chapter_#{@title_number}_#{@chapter_number}.wav")
+      return @wav if @wav
+
+ 
