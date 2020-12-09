c10e3b9617762c482a24ad1ef1707770ec4c0bbb
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2020-12-09 08:06:44 +0000

    tracing/filter: Do not WARN on operand count going below zero

--- lib/musical/notification/progress_bar.rb
+++ lib/musical/notification/progress_bar.rb
@@ -1,2 +1,22 @@
 
 # coding: utf-8
+module Musical
+  module Notification
+    class ProgressBar < ::ProgressBar
+      FORMAT = '%a %B %p%% %t'
+
+      def self.create(options = {})
+        options = { format: FORMAT }.merge(options)
+        progress_bar = super(options)
+
+        Thread.new do
+          while !progress_bar.finished?
+            progress_bar.refresh
+          end
+        end
+        progress_bar
+      end
+
+    end
+  end
+end
---
+++ lib/musical/util.rb
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+module Musical
+  module Util
+    REQUIRED_APPS = %w(dvdbackup ffmpeg).freeze
+
+    def check_env
+      REQUIRED_APPS.each do |app|
+        unless installed?(app)
+          messages = []
+          messages << "'#{app}' is not installed."
+          messages << "Try this command to install '#{app}'."
+          messages << ""
+          messages << "   brew install #{app}"
+          messages << ""
