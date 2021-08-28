d6fa20b7b58165000594cc3a8edc7f559759c240
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2021-08-28 16:12:13 +0000

    Added category

--- spec/musical/configuration_spec.rb
+++ spec/musical/configuration_spec.rb
@@ -7,4 +7,4 @@ describe Musical::Configuration do
     subject { described_class.build(options) }
     let(:options) { { title: 'foo', artist: 'bar' } }
 
-    it 'sets hash data to con
+    it 'sets hash data to config class varible as subclass of Op
