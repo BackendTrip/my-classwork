fceb34bdf4c63d520de0fcb2397e2bf7171582b5
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2021-09-04 14:19:44 +0000

    Create codecov.yml

--- spec/musical/configuration_spec.rb
+++ spec/musical/configuration_spec.rb
@@ -11,4 +11,6 @@ describe Musical::Configuration do
       subject
       expect(described_class.config).to be_an OpenStruct
       expect(subject.title).to eq('foo')
-      expect(subject.artist).to e
+      expect(subject.artist).to eq('bar')
+    end
+  end
