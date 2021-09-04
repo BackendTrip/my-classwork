d9a5f8e13978a8f18cc298de1747964425462f40
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2021-09-04 18:02:42 +0000

    Update load.js

--- spec/musical/dvd/chapter_spec.rb
+++ spec/musical/dvd/chapter_spec.rb
@@ -24,3 +24,25 @@ describe Musical::DVD::Chapter do
       end
     end
   end
+
+  describe '#to_wav' do
+    subject { chapter.to_wav(wav_path) }
+
+    let(:chapter) { described_class.new(vob_path, chapter_number: 10) }
+    let(:vob_path) { '/path/to/foo.vob' }
+    let(:wav_path) { '/tmp/foo.wav' }
+
+    before do
+      expect(chapter).to receive(:execute_command).
+        with("ffmpeg -i #{vob_path} -ac 2 #{wav_path}", true).
+        and_return(FileUtils.touch(wav_path))
+    end
+
+    after { subject.delete! }
+
+    it 'returns wav file which is converted', fakefs: true do
+      expect(subject).to be_a(Musical::DVD::Wav)
+      expect(subject.path).to eq(wav_path)
+    end
+  end
+end
