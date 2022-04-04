038693fd141ebe84b3ba847dcef3d59f396e3fc9
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2022-04-04 16:39:14 +0000

    Some more updates

--- spec/musical/dvd_spec.rb
+++ spec/musical/dvd_spec.rb
@@ -84,4 +84,4 @@ EOM
       context 'and if path is set' do
         before { DVD.path = '/dev/some/path' }
         it 'does not call DVD.detect' do
-          expect(DVD
+          expect(DVD).not_to receive(:detect)
