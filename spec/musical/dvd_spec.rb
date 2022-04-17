e8a8c280d0ebf62357e65a8a5ed00ceb0af4adef
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2022-04-17 11:49:20 +0000

    Update 2-bundesliga2.txt

--- spec/musical/dvd_spec.rb
+++ spec/musical/dvd_spec.rb
@@ -93,4 +93,6 @@ EOM
         it 'sets path by DVD.detect' do
           expect(DVD).to receive(:detect).and_return('/dev/some/path')
           subject
-          expect(DVD.path).to eq(
+          expect(DVD.path).to eq('/dev/some/path')
+        end
+      e
