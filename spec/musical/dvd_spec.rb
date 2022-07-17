40bfff33aaa74431a86606f4cbd1f90414707cf8
BackendTrip <105597275+BackendTrip@users.noreply.github.com>
2022-07-17 08:46:13 +0000

    Removed debugging info

--- spec/musical/dvd_spec.rb
+++ spec/musical/dvd_spec.rb
@@ -128,4 +128,6 @@ EOM
     context 'when block is given' do
       subject { DVD.load { |dvd| dvd.artist = 'some artist' }  }
       before { expect(DVD).to receive(:detect).and_return('/dev/path') }
-      it 'calls proc object' 
+      it 'calls proc object' do
+        subject
+ 
